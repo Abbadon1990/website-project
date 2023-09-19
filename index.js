ab6a668226532a65142f9c682a45dad7b35bb63b
Logan Wood <lgnwood@gmail.com>
2023-09-19 04:20:43 -0600

    add css styling

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content add by JavaScript";
+h2.textContent = "The goal of this website is to self host throguh GitHub and learn how to incorporate CSS and JavaScript with HTML.";
 document.querySelector("body").appendChild(h2);
